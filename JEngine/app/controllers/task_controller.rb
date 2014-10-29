96298f2b461e76dfd7378c0da18d731bc9d0b0de
jaSunny <github@sunny.remote.mx>
2014-10-29 23:29:11 +0100

    PA-57 #comment: implements db getter and setter for completing a task on stage table

--- JEngine/app/controllers/task_controller.rb
+++ JEngine/app/controllers/task_controller.rb
@@ -1,15 +1,21 @@
 class TaskController < ApplicationController
   def new
-	puts 'viewing index'
+	#puts 'viewing index'
   end
   def done
-	puts 'done editing'
+	#puts 'done editing'
   end
   def completed
       cookie_id = cookies[:cookie_id]
       task_id = params[:task_id]
-      puts "finished task: cookie  task_id= #{task_id} cookie_id= #{cookie_id}"
-      
+      task_id = task_id.keys.to_s
+      #updates db record
+      foo = Stages.find_by_user_id(cookie_id)
+      if !foo
+         foo = Stages.create(user_id: cookie_id, enabled: '', completed: '')
+      end
+      foo = Stages.where(:user_id => cookie_id).update_all(:completed => task_id)
+
       redirect_to :action => 'done'
   end
 end

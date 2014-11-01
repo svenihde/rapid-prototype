655d2e60a3973fb547f890ff7e03b5a0d0ce9691
jaSunny <github@sunny.remote.mx>
2014-11-01 14:12:18 +0100

    PA-63 #comment: adding views

--- JEngine/app/controllers/activity_controller.rb
+++ JEngine/app/controllers/activity_controller.rb
@@ -4,10 +4,17 @@ class ActivityController < ApplicationController
   end
   def create
 	render plain: params[:activity].inspect
-	foo = Activites.create(title: title, description: description)	
-	task_id = Activity.order("update_at DESC").limit(1)
-
-	puts 'you added the activity with the task_id #{task_id}'
+	foo = Activites.create(title: title, description: description)
+        if !foo
+	 	puts 'error during adding new activity into DB'
+        end
+        
+        redirect_to :action => 'done'
+	
+	#task_id = Activity.order("update_at DESC").limit(1)
+	#puts 'you added the activity with the task_id #{task_id}'
+  end
+  def done
   end
   def edit
 	#task_id =

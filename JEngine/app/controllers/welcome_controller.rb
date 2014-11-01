61914da18bf8c8e7550ab9d9151232b0c6cc6940
jaSunny <github@sunny.remote.mx>
2014-11-01 14:42:10 +0100

    PA-63 #comment: more fixes, now displaying the completed activities for each user via cookie_id

--- JEngine/app/controllers/welcome_controller.rb
+++ JEngine/app/controllers/welcome_controller.rb
@@ -1,12 +1,13 @@
 class WelcomeController < ApplicationController
   def index
 	if cookies[:cookie_id] 
-		show_cookies	
+		show_cookies
 	else	
 		puts "user unknown - setting cookie"
 		set_cookies
 	end
 	@activity_list = Activity.all
+	show_completed_activites
   end
   def set_cookies
     cookies[:cookie_id] = rand(999999) 
@@ -20,5 +21,8 @@ class WelcomeController < ApplicationController
   
   def delete_cookies
     cookies.delete :cookie_id
+  end
+  def show_completed_activites
+      @stages_of_cookie_id = Stages.where(user_id: cookies[:cookie_id] )
   end
 end

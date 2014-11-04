1273730cfe08be3c2a129512fef792c3eccec2e8
jaSunny <jannik@github.com>
2014-11-04 10:22:19 +0100

    adding DOM XML Parser & Apache file upload

--- JCompaser/src/Connector.java
+++ JCompaser/src/Connector.java
@@ -1,6 +1,7 @@
 /**
  * Created by Jani on 04.11.2014.
  */
+
 public class Connector {
     /* Establish a connection with DB through JDBC */
 
--- JCompaser/src/main.java
+++ JCompaser/src/main.java
@@ -1,10 +1,68 @@
 /**
  * Created by Jani on 04.11.2014.
  */
+package org.kodejava.example.commons.fileupload;
+
+import org.apache.commons.fileupload.servlet.ServletFileUpload;
+import org.apache.commons.fileupload.FileItemFactory;
+import org.apache.commons.fileupload.FileUploadException;
+import org.apache.commons.fileupload.FileItem;
+import org.apache.commons.fileupload.disk.DiskFileItemFactory;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.ServletException;
+import java.io.IOException;
+import java.io.File;
+import java.util.List;
+import java.util.Iterator;
+
 public class main {
     /* read the XML file, parse it and wrap it up for engine use and write it to DB */
 
     /* @TODO: do we want a webbased application -> makes sense
              therefor we need a webform in order to upload the file
      */
+}
+
+
+public class FileUploadDemoServlet extends HttpServlet {
+    private static final long serialVersionUID = -3208409086358916855L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        boolean isMultipart = ServletFileUpload.isMultipartContent(request);
+
+        if (isMultipart) {
+            FileItemFactory factory = new DiskFileItemFactory();
+            ServletFileUpload upload = new ServletFileUpload(factory);
+
+            try {
+                List items = upload.parseRequest(request);
+                Iterator iterator = items.iterator();
+                while (iterator.hasNext()) {
+                    FileItem item = (FileItem) iterator.next();
+
+                    if (!item.isFormField()) {
+                        String fileName = item.getName();
+
+                        String root = getServletContext().getRealPath("/");
+                        File path = new File(root + "/uploads");
+                        if (!path.exists()) {
+                            boolean status = path.mkdirs();
+                        }
+
+                        File uploadedFile = new File(path + "/" + fileName);
+                        System.out.println(uploadedFile.getAbsolutePath());
+                        item.write(uploadedFile);
+                    }
+                }
+            } catch (FileUploadException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }

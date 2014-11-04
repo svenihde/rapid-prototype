f3848297ef1b74a7ab86a364de7be1f747d6e542
jaSunny <jannik@github.com>
2014-11-04 08:50:04 +0100

    prepare basic Java framework

---
+++ JCompaser/src/Parser.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Jani on 04.11.2014.
+ */
+public class Parser {
+    /* parse the XML File*/
+
+    /* @TODO: include dom XML parser
+              maybe think about alternatives which provides prepaired objects
+     */
+}
---
+++ JCompaser/src/main.java
@@ -0,0 +1,10 @@
+/**
+ * Created by Jani on 04.11.2014.
+ */
+public class main {
+    /* read the XML file, parse it and wrap it up for engine use and write it to DB */
+
+    /* @TODO: do we want a webbased application -> makes sense
+             therefor we need a webform in order to upload the file
+     */
+}

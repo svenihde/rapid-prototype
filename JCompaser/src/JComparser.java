c43b9a12622ebee0a01841f110ccc26d1eb529bf
jaSunny <jannik@github.com>
2014-11-10 13:03:08 +0100

    PA-85 #comments: building a package

--- JCompaser/src/JComparser.java
+++ JCompaser/src/JComparser.java
@@ -23,7 +23,7 @@ public class JComparser {
 	public static void main(String[] args) {
 
 		// TODO Auto-generated method stub
-		JDBCHandler jHandler = new JDBCHandler();
+		//JDBCHandler jHandler = new JDBCHandler();
 		try {
 			File BPMNXML = new File("./" + args[1]);
 			DocumentBuilderFactory dbFactory = DocumentBuilderFactory

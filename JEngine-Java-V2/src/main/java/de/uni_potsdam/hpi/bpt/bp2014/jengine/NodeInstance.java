f6d652d14f0a84c7339c67a08190320a5c8310b0
Jaspar Mang <jaspar.mang@student.hpi.uni-potsdam.de>
2014-11-24 15:20:00 +0100

    PA-100 #comments: created more classes and mangaged package structure

---
+++ JEngine-Java-V2/src/main/java/de/uni_potsdam/hpi/bpt/bp2014/jengine/NodeInstance.java
@@ -0,0 +1,10 @@
+package de.uni_potsdam.hpi.bpt.bp2014.jengine;
+
+/**
+ * Created by jaspar.mang on 24.11.14.
+ */
+public class NodeInstance {
+    private OutgoingBehavior outgoingBehavior;
+    private IncomingBehavior incomingBehavior;
+    private StateMachine stateMachine;
+}

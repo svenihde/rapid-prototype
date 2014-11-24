3e21f468fbcf51f6f26df013b22bf39ddf94255a
jaSunny <jannik@github.com>
2014-11-24 14:29:04 +0100

    first bootstrap for the JComparser

---
+++ JComparser-REST/src/main/java/Connector.java
@@ -0,0 +1,52 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ * Created by Jani on 24.11.2014.
+ */
+public class Connector {
+        /* @TODO: how to handle duplicated entry errors? */
+
+    // JDBC driver name and database URL
+    static final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
+    static final String DB_URL = "jdbc:mysql://localhost:3306/JEngine";
+
+    // Database credentials
+    static final String USER = "root";
+    static final String PASS = "samsa";
+    public static Connection connect() {
+        Connection conn = null;
+        try {
+            //Register JDBC driver
+            Class.forName("com.mysql.jdbc.Driver");
+
+            //Open a connection
+
+            conn = DriverManager.getConnection(DB_URL, USER, PASS);
+
+        } catch (SQLException se) {
+            //Handle errors for JDBC
+            System.out.println("hier");
+            se.printStackTrace();
+        } catch (Exception e) {
+            //Handle errors for Class.forName
+            System.out.println("da");
+
+            e.printStackTrace();
+        }
+        return conn;
+    }
+
+    public void insertScenarioIntoDatabase(String name, String ter) {
+    }
+
+    public void insertGatewayIntoDatabase(String textContent) {
+    }
+
+    public void insertEventIntoDatabase(String textContent) {
+    }
+
+    public void insertActivityIntoDatabase(String name) {
+    }
+}
---
+++ JComparser-REST/src/main/java/JComparser.java
@@ -0,0 +1,9 @@
+/**
+ * Created by Jani on 24.11.2014.
+ */
+public class JComparser {
+    public static void main() {
+
+        Connector jHandler = new Connector();
+    }
+}
